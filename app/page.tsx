6d7fc44d00527c4b39c16040bf179a3a2faac5fc
Emmet Brickowski <thespecial8421@gmail.com>
2025-02-20 15:28:45 -0600

    More changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import React, { useState, useEffect } from 'react';
 import Image from "next/image";
